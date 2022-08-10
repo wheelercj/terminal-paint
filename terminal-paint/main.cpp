1c55ef3e18d483ae31102e2782f9f118d8347c87
wheelercj <chrisw289@gmail.com>
2022-08-10 12:05:01 -0700

    Add a main menu

--- terminal-paint/main.cpp
+++ terminal-paint/main.cpp
@@ -8,20 +8,109 @@ using ynot::Coord;
 HANDLE hStdin;
 DWORD fdwSaveOldMode;
 
+bool confirmed_dont_save();
+void notify(string message, Coord window_size);
+void show_help(Coord window_size);
+bool open_canvas(vector<vector<string>>& canvas, Coord window_size, string& drawing_character, int& drawing_radius);
 vector<vector<string>> init_canvas(Coord window_size);
-void print_entire_canvas(vector<vector<string>> canvas, Coord window_size);
-void show_menu(string& drawing_character, int& drawing_radius);
+void clear_canvas(vector<vector<string>>& canvas);
+void load_canvas(vector<vector<string>>& canvas, Coord window_size);
+void save_canvas(vector<vector<string>>& canvas);
+void print_entire_canvas(vector<vector<string>>& canvas, Coord window_size);
+bool show_canvas_menu(string& drawing_character, int& drawing_radius);
 void draw(string output, COORD cursor_coord, int radius, vector<vector<string>>& canvas, Coord window_size);
 void error_exit(string message);
 void reset_terminal();
 
+
 int main()
 {
-	bool just_started = true;
 	ynot::alternate_screen_buffer();
-	ynot::set_cursor_style(CursorStyle::hidden);
+	ynot::reset_on_keyboard_interrupt();
+	ynot::Menu main_menu("terminal paint", { "paint", "help", "exit" });
+	//ynot::Menu main_menu("terminal paint", { "paint", "load", "save", "clear", "help", "exit" });
 	Coord window_size = ynot::get_window_size();
 	vector<vector<string>> canvas = init_canvas(window_size);
+	int drawing_radius = 1;
+	string drawing_character = "█";
+
+	string choice = "";
+	bool saved = true;
+	while (choice != "exit" /*|| (!saved && !confirmed_dont_save())*/)
+	{
+		choice = main_menu.run();
+		ynot::alternate_screen_buffer();
+		if (choice == "paint")
+		{
+			if (open_canvas(canvas, window_size, drawing_character, drawing_radius))
+				saved = false;
+		}
+		else if (choice == "load" && (saved || confirmed_dont_save()))
+		{
+			load_canvas(canvas, window_size);
+			if (open_canvas(canvas, window_size, drawing_character, drawing_radius))
+				saved = false;
+		}
+		else if (choice == "save")
+		{
+			save_canvas(canvas);
+			saved = true;
+			notify("Canvas saved.", window_size);
+		}
+		else if (choice == "clear" && (saved || confirmed_dont_save()))
+		{
+			clear_canvas(canvas);
+			saved = true;
+			notify("Canvas cleared.", window_size);
+		}
+		else if (choice == "help")
+		{
+			show_help(window_size);
+		}
+	}
+
+	reset_terminal();
+	return 0;
+}
+
+bool confirmed_dont_save()
+{
+	ynot::Menu confirmation_menu("Save changes?", { "save", "don't save", "cancel" });
+	string choice = confirmation_menu.run();
+	ynot::alternate_screen_buffer();
+	if (choice == "don't save")
+		return true;
+	else
+		return false;
+}
+
+void notify(string message, Coord window_size)
+{
+	ynot::clear_screen();
+	ynot::set_cursor_coords(1, 3);
+	ynot::print(ynot::dedent(message));
+	ynot::print("\n\n     Press any key to continue.");
+	ynot::pause();
+}
+
+void show_help(Coord window_size)
+{
+	notify(R"(
+		In the paint canvas:
+		• press tab to open the brush selection menu 
+		  and any other key to close it
+		• press escape to return to the main menu
+		• left click to draw and right click to erase
+		• use the number keys to control the brush radius
+		• for more help, join the discussions at
+		  https://github.com/wheelercj/terminal-paint/discussions
+		)", window_size);
+}
+
+/* Returns true if the canvas changed, false otherwise. */
+bool open_canvas(vector<vector<string>>& canvas, Coord window_size, string& drawing_character, int& drawing_radius)
+{
+	bool canvas_changed = false;
 	DWORD cNumRead, fdwMode, i;
 	INPUT_RECORD irInBuf[128];
 
@@ -38,8 +127,7 @@ int main()
 	if (!SetConsoleMode(hStdin, fdwMode))
 		error_exit("SetConsoleMode");
 
-	int drawing_radius = 1;
-	string drawing_character = "█";
+	bool just_started = true;
 	while (true)
 	{
 		if (!ReadConsoleInput(
@@ -68,33 +156,30 @@ int main()
 				{
 				case 1:
 					draw(drawing_character, coord, drawing_radius, canvas, window_size);
+					canvas_changed = true;
 					break;
 				case 2:
 					draw(" ", coord, drawing_radius, canvas, window_size);
+					canvas_changed = true;
 					break;
 				}
 				break;
 			case KEY_EVENT:
 				WCHAR key = irInBuf[i].Event.KeyEvent.uChar.UnicodeChar;
 				if (key == '\x1b')  // escape
-				{
-					reset_terminal();
-					return 0;
-				}
+					return canvas_changed;
 				if (key >= '1' and key <= '9')
 					drawing_radius = key - '0';
 				else if (key == '\t')
 				{
-					show_menu(drawing_character, drawing_radius);
+					if (!show_canvas_menu(drawing_character, drawing_radius))
+						return canvas_changed;
 					ynot::clear_screen();
 					print_entire_canvas(canvas, window_size);
 				}
 			}
 		}
 	}
-
-	reset_terminal();
-	return 0;
 }
 
 vector<vector<string>> init_canvas(Coord window_size)
@@ -110,7 +195,26 @@ vector<vector<string>> init_canvas(Coord window_size)
 	return canvas;
 }
 
-void print_entire_canvas(vector<vector<string>> canvas, Coord window_size)
+void clear_canvas(vector<vector<string>>& canvas)
+{
+	for (size_t y = 0; y < canvas.size(); y++)
+	{
+		for (size_t x = 0; x < canvas[y].size(); x++)
+			canvas[y][x] = " ";
+	}
+}
+
+void load_canvas(vector<vector<string>>& canvas, Coord window_size)
+{
+	// TODO
+}
+
+void save_canvas(vector<vector<string>>& canvas)
+{
+	// TODO
+}
+
+void print_entire_canvas(vector<vector<string>>& canvas, Coord window_size)
 {
 	for (int y = 0; y < window_size.y; y++)
 	{
@@ -119,18 +223,15 @@ void print_entire_canvas(vector<vector<string>> canvas, Coord window_size)
 	}
 }
 
-void show_menu(string& drawing_character, int& drawing_radius)
+/* Returns true if returning to the canvas, false if returning to the main menu. */
+bool show_canvas_menu(string& drawing_character, int& drawing_radius)
 {
 	ynot::clear_screen();
-	ynot::reset_on_keyboard_interrupt();
-	ynot::print_at(1, 1, ynot::dedent(R"help_menu(
-        Pres tab to open this menu and any other key to close it.
-        Press escape to close the app (changes are not saved!).
-        Outside this menu, left click to draw and right click to erase.
-        You can hold shift and use the mouse to select parts of
-        the screen and then press ctrl+c to copy.
-
-        Use the number keys to control drawing width.
+	ynot::print_at(1, 1, ynot::dedent(R"canvas_menu(
+        Press tab to open this menu and any other key to close it.
+        Press escape to return to the main menu.
+        In the canvas, left click to draw and right click to erase.
+        The number keys control the brush radius.
 
         Press a key to choose a symbol to draw with:
 
@@ -142,21 +243,18 @@ void show_menu(string& drawing_character, int& drawing_radius)
 
         █ ▓ ▒ ░ ▄ ▀ ■ ≡ . ·
         z x c v b n m , . /
-        )help_menu"));
+        )canvas_menu"));
 	string key = "tab";
 	while (key == "tab")
 	{
 		key = ynot::get_key();
 		if (key == "escape")
-		{
-			reset_terminal();
-			exit(0);
-		}
+			return false;
 		try
 		{
 			drawing_radius = stoi(key);
 		}
-		catch (std::invalid_argument){}
+		catch (std::invalid_argument) {}
 		catch (std::out_of_range) {}
 		if (key == "q")
 			drawing_character = "│";
@@ -225,6 +323,8 @@ void show_menu(string& drawing_character, int& drawing_radius)
 		else if (key == "/")
 			drawing_character = "·";
 	}
+
+	return true;
 }
 
 void draw(string output, COORD cursor_coord, int radius, vector<vector<string>>& canvas, Coord window_size)
@@ -233,7 +333,7 @@ void draw(string output, COORD cursor_coord, int radius, vector<vector<string>>&
 		for (int y = cursor_coord.Y - radius + 1; y <= cursor_coord.Y + radius - 1; y++) {
 			if (window_size.y > y && window_size.x > x && y >= 0 && x >= 0)
 			{
-				canvas[y + 1][x + 1] = output;
+				canvas[size_t(y) + 1][size_t(x) + 1] = output;
 				ynot::print_at(x + 1, y + 1, output);
 			}
 		}
